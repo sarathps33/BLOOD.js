41bf6af8fc95ca6054627ecff8303d411931cc7c
sarathps33 <sarathps224@gmail.com>
2024-09-04 13:54:13 +0530

    add

--- server_side/server.js
+++ server_side/server.js
@@ -10,12 +10,34 @@ const app=http.createServer((req,res)=>{
     }
     else if (newUrl.pathname=="/js/sarath.js") {
         res.writeHead(200, { "Content-type": "text/js"});
-        res.end(fs.readFileSync("/js/sarath.js"))
+        res.end(fs.readFileSync("../client_server/js/sarath.js"))
     }
-    else if (newUrl.pathname=="/add") {
+    else if (newUrl.pathname=="/pages/dono.html") {
         res.writeHead(200, { "Content-type": "text/html"});
-        res.end(fs.readFileSync("/pages/dono.htm"))
+        res.end(fs.readFileSync("../client_server/pages/dono.html"))
     }
+    else if (newUrl.pathname=="/css/style.css") {
+        res.writeHead(200, { "Content-type": "text/css"});
+        res.end(fs.readFileSync("../client_server/css/style.css"))
+    }
+    else if (newUrl.pathname=="/css/donor.css") {
+        res.writeHead(200, { "Content-type": "text/css"});
+        res.end(fs.readFileSync("../client_server/css/donor.css"))
+    }
+    if(req.method == "post" && path.pathname =="/submit"){
+        let body=""
+    req.on("data",(fake)=>{
+        console.log(fake);
+        body +=fake.toString();
+        console.log(body);
+        
+                
+    })
+}
+  
+
+
+
 
 
 
